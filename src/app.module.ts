827d9d56c6852c4b65ad296382a93d2f0ed830ae
syedubaid <syedabuubaid@gmail.com>
2025-09-10 01:25:40 +0530

    feat : added cache modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CacheModule } from './cache/cache.module';
+import { CachController } from './cach/cach.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [CacheModule],
+  controllers: [AppController, CachController],
   providers: [AppService],
 })
 export class AppModule {}
